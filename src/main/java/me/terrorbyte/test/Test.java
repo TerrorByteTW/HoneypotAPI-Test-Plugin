adc7b4d04d4d1f5d434513b003de5f3ce1239902
Nate Reprogle <nate.reprogle@gmail.com>
2022-07-05 21:15:53 -0500

    Update Test Plugin to show examples from Honeypot API v2.1.1

--- src/main/java/me/terrorbyte/test/Test.java
+++ src/main/java/me/terrorbyte/test/Test.java
@@ -7,8 +7,9 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import me.terrorbyte.honeypot.api.events.HoneypotPrePlayerBreakEvent;
-import me.terrorbyte.honeypot.api.events.HoneypotPrePlayerInteractEvent;
+import org.reprogle.honeypot.api.events.HoneypotPrePlayerBreakEvent;
+import org.reprogle.honeypot.api.events.HoneypotPrePlayerInteractEvent;
+import org.reprogle.honeypot.storagemanager.HoneypotBlockManager;
 
 public class Test extends JavaPlugin implements Listener {
     
@@ -20,6 +21,7 @@ public class Test extends JavaPlugin implements Listener {
         plugin = this;
         this.getLogger().info("Enabling!");
         getCommand("testcommand").setExecutor(this);
+        getCommand("checkblock").setExecutor(this);
         this.getServer().getPluginManager().registerEvents(this, this);
     }
 
@@ -48,15 +50,38 @@ public class Test extends JavaPlugin implements Listener {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (testActive) {
-            testActive = false;
-            ((Player) sender).sendMessage("Off!");
-        } else {
-            testActive = true;
-            ((Player) sender).sendMessage("On!");
-        }
 
-        return true;
+        // Enable event cancelling
+        if(label.equalsIgnoreCase("testcommand")) {
+            if (testActive) {
+                testActive = false;
+                ((Player) sender).sendMessage("Honeypot event cancelling is off!");
+                return true;
+            } else {
+                testActive = true;
+                ((Player) sender).sendMessage("Honeypot event cancelling is on!");
+                return true;
+            }
+        
+        // Use the HoneypotBlockManager API to check for Honeypot Blocks. This is just an example, refer to the Javadoc for more info
+        } else if (label.equalsIgnoreCase("checkblock")) {
+            Player player = (Player) sender;
+
+            // Create a HoneypotBlockManager object to manage Honeypot Blocks safely
+            HoneypotBlockManager hbm = new HoneypotBlockManager();
+
+            // If the target block exactly 5 blocks away is a Honeypot, let the player no
+            if(hbm.isHoneypotBlock(player.getTargetBlockExact(5))) {
+                player.sendMessage("I'm a Honeypot! :)");
+                return true;
+            } else {
+                player.sendMessage("I'm not a Honeypot :(");
+                return true;
+            }
+        }
+        
+
+        return false;
     }
 
 }
