d850674ff7807b0dbf803d7a7da610137958dd1e
Nate Reprogle <nate.reprogle@gmail.com>
2026-04-13 16:02:37 -0500

    * Update to support changes to HoneypotStore in Honeypot 4.0.0

--- src/main/java/me/terrorbyte/test/DemoHoneypotStore.java
+++ src/main/java/me/terrorbyte/test/DemoHoneypotStore.java
@@ -1,10 +1,13 @@
 package me.terrorbyte.test;
 
 import org.bukkit.Location;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.World;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.Nullable;
 import org.reprogle.honeypot.common.storageproviders.HoneypotBlockObject;
+import org.reprogle.honeypot.common.storageproviders.HoneypotPlayerHistoryObject;
 import org.reprogle.honeypot.common.storageproviders.HoneypotStore;
 import org.reprogle.honeypot.common.storageproviders.StorageProvider;
 
@@ -56,5 +59,53 @@ public class DemoHoneypotStore extends StorageProvider {
     public List<HoneypotBlockObject> getNearbyHoneypots(Location location, int i) {
         Logger.getLogger("minecraft").info("Get nearby honeypot blocks was called!");
         return List.of();
+    }
+
+    @Override
+    public void addPlayer(Player player, int i) {
+        Logger.getLogger("minecraft").info("Add player was called!");
+    }
+
+    @Override
+    public void setPlayerCount(Player player, int i) {
+        Logger.getLogger("minecraft").info("Set player count was called!");
+    }
+
+    @Override
+    public int getCount(Player player) {
+        Logger.getLogger("minecraft").info("Get player count was called!");
+        return 0;
+    }
+
+    @Override
+    public int getCount(OfflinePlayer offlinePlayer) {
+        Logger.getLogger("minecraft").info("Get player count was called!");
+        return 0;
+    }
+
+    @Override
+    public void deleteAllHoneypotPlayers() {
+        Logger.getLogger("minecraft").info("Delete all honeypot players was called!");
+    }
+
+    @Override
+    public void addPlayerHistory(Player player, HoneypotBlockObject honeypotBlockObject, String s) {
+        Logger.getLogger("minecraft").info("Add player history was called!");
+    }
+
+    @Override
+    public List<HoneypotPlayerHistoryObject> getPlayerHistory(Player player) {
+        Logger.getLogger("minecraft").info("Get player history was called!");
+        return List.of();
+    }
+
+    @Override
+    public void deletePlayerHistory(Player player, int... ints) {
+        Logger.getLogger("minecraft").info("Delete player history was called!");
+    }
+
+    @Override
+    public void deleteAllHistory() {
+        Logger.getLogger("minecraft").info("Delete all history was called!");
     }
 }
